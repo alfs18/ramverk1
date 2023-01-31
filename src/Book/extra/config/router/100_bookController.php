e0c96bac94dce09b9023402abb07bb3bcebdea15
alfs18 <44801730+alfs18@users.noreply.github.com>
2023-01-31 12:33:32 +0100

    Add files via upload

---
+++ src/Book/extra/config/router/100_bookController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Routes for controller.
+ */
+return [
+    "routes" => [
+        [
+            "info" => "Controller for book.",
+            "mount" => "book",
+            "handler" => "\Alfs18\Book\BookController",
+        ],
+    ]
+];
